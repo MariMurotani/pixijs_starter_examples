16bf0042ce593ae1cb450728466c37058645edce
Mari Murotani <muromari5123@gmail.com>
2016-10-08 15:54:55 +0900

    Added https://github.com/noprotocol/gsap-pixi-plugin

--- app/assets/javascripts/pixijs.js
+++ app/assets/javascripts/pixijs.js
@@ -4,6 +4,7 @@
 //= require pixijs/jquery-3.1.1.min
 //= require pixijs/pixi.min
 //= require pixijs/pixi-particles
+//= require pixijs/PixiPlugin
 //= require pixijs/stats
 //= require pixijs/alias
 //= require pixijs/resources
